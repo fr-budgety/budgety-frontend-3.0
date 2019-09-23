353c1ce5228e5897e2e0b7f0a89c320ea8ec1b4c
frivolta <rivoltafilippo@gmail.com>
2019-09-23 21:27:07 +0200

    :sparkles: Adding redux, thunk

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import "./assets/vendor/nucleo/css/nucleo.css";
 import "./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/scss/dashboard.scss";
+
 import AuthenticationPage from './views/AuthenticationPage';
+import rootReducer from './redux/reducers/rootReducer';
+
+// Create redux store
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk)
+));
 
 
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path="/auth/register" render={props => <AuthenticationPage {...props} />} />
-      <Route path="/" render={props => <AuthenticationPage {...props} />} />
-    </Switch>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/auth/register" render={props => <AuthenticationPage {...props} />} />
+        <Route path="/" render={props => <AuthenticationPage {...props} />} />
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
